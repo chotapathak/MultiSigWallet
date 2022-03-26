3ebb2f961579afdef0d24078292d83cf5c36b018
Vikash Pathak <chotapathak12@gmail.com>
2022-03-26 20:53:09 +0530

    multisig contract

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.3;
+import "../contracts/IMSIG.sol";
 
-contract MultiSigWallet {
+contract MultiSigWallet is IMSIG{
     // limiting owner storage
     uint constant public MAX_OWNER_COUNT = 50;
 
-    // important events
+//     // important events
     event Confirmation(address indexed sender, uint indexed transactionId);
     event Revocation(address indexed sender, uint indexed transactionId);
     event Submission(uint indexed transactionId);
@@ -16,9 +17,10 @@ contract MultiSigWallet {
     event OwnerRemoval(address indexed owner);
     event RequirementChange(uint required);
 
-    address[] public owners;
-    uint public required;
-    uint public transactionCount;
+    address[] public  owners;
+    // address public override _owner;
+    // uint public override required;
+    // uint public override transactionCount;
 
     struct Transaction {
       address destination;
@@ -28,46 +30,25 @@ contract MultiSigWallet {
     }
     mapping(uint => Transaction) public transactions;
 
-    modifier onlyWallet() 
+    modifier validRequirement(uint ownerCount, uint _required)
     {
-      require(msg.sender != address(this)); 
-            revert();
-       _;
-    }
-//
-    mapping(address => bool) isOwner;
-    modifier ownerDoesNotExist(address owner) 
-    {
-      if(isOwner[owner]) revert();
-      _;
+        if(ownerCount > MAX_OWNER_COUNT || _required > ownerCount ||
+                _required == 0 || ownerCount == 0 ) revert();
+        _;
     }
 
-    modifier ownerExists(address owner) 
+    modifier onlyOwner()
     {
-      if (!isOwner[owner])
-      _;
+        require(msg.sender == _owner, 'not an owner');
+        _;
     }
-//    mapping(uint => Transaction) public transactions;
-    modifier trasactionExists(uint transactionId) 
+// 
+    modifier transactionExists(uint transactionId) 
     {
       if(transactions[transactionId].destination == address(0)) revert();
       _;
     }
-//
-    mapping(uint => mapping(address => bool)) public confirmations;
-
-    modifier confirmed(uint transactionId, address owner) 
-    {
-      if (!confirmations[transactionId][owner]) revert();
-      _;
-    }
-
-    modifier notConfirmed(uint transactionId, address owner)
-    {
-        if(confirmations[transactionId][owner]) revert();
-        _;
-    }
-
+// 
     modifier notExecuted(uint transactionId)
     {
         if(transactions[transactionId].executed) 
@@ -81,14 +62,14 @@ contract MultiSigWallet {
         _;
     }
 
-    modifier validRequirement(uint ownerCount, uint _required)
-    {
-        if(ownerCount > MAX_OWNER_COUNT || _required > ownerCount ||
-                _required == 0 || ownerCount == 0 ) revert();
-        _;
-    }
     // fallback function allows to deposit ether
     fallback() external payable
+    {
+        if(msg.value > 0)
+        emit Deposit(msg.sender, msg.value);
+    }
+    // Recieve fallback function
+    receive() external payable
     {
         if(msg.value > 0)
         emit Deposit(msg.sender, msg.value);
@@ -109,15 +90,15 @@ contract MultiSigWallet {
             owners = _owners;
             required = _required;
         }
-    // This add new owner. then send Transaction should be sent owner Address of new owner
-    function addOwner(address owner) public onlyWallet ownerDoesNotExist(owner)
-        notNull(owner)
-        validRequirement(owners.length + 1, required)
-        {
-            isOwner[owner] = true;
-            owners.push(owner);
-            emit OwnerAddition(owner);
-        }
+    // // This add new owner. then send Transaction should be sent owner Address of new owner
+    // function addOwner(address owner) public onlyWallet ownerDoesNotExist(owner)
+    //     notNull(owner)
+    //     validRequirement(owners.length + 1, required)
+    //     {
+    //         isOwner[owner] = true;
+    //         owners.push(owner);
+    //         emit OwnerAddition(owner);
+    //     }
 
     // allows to change number of required confirmations
         function changeRequirement(uint _required) 
@@ -126,8 +107,24 @@ contract MultiSigWallet {
             validRequirement(owners.length, _required)
         {
             required = _required;
-            RequirementChange(_required);
+            emit RequirementChange(_required);
         }   
+
+    function delOwner(address owner) public onlyWallet {
+        bool alreadyDeleted = false;
+        uint ownerIndex;
+        for (uint i =0; i < owners.length; i++) {
+            if (owners[i] == owner) {
+                alreadyDeleted = true;
+                ownerIndex = i;
+                break;
+            }
+        }
+        require(alreadyDeleted == true, ' owner not detected');
+        owners[ownerIndex] = owners[owners.length - 1];
+        owners.pop();
+        emit OwnerRemoval(owner);
+    }    
     // Remove Owner
     function removeOwner(address owner)
         public
@@ -140,9 +137,53 @@ contract MultiSigWallet {
                     owners[i] = owners[owners.length - 1];
                     break;
                 }
-            owners.length -= 1;
+                owners.pop();
             if (required > owners.length)
                 changeRequirement(owners.length);
             emit OwnerRemoval(owner);        
         }         
+    // deposit function
+        uint balances;
+    mapping(address => uint) balance;
+    function deposit(uint amount) public payable ownerExists(_owner) {
+        require(msg.value >= amount, 'deposit amount must be greater than or equal to the amount');
+        require(amount > 0, 'deposit amount must be greater than 0');
+        // require(msg.value > .01 ether);
+        require(msg.sender == _owner, 'only main owner can deposit');
+        require(balance[msg.sender] >= msg.value, 'insufficient balance');
+        balance[msg.sender] = msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }    
+    //
+    function addTransaction(address destination,uint value, bytes memory data) 
+        internal 
+        notNull(destination)
+        returns (uint transactionId)
+    {
+        transactionId = transactionCount;
+        transactions[transactionId] = Transaction({
+            destination:  destination,
+            value: value,
+            data: data,
+            executed: false
+        });
+    } 
+    //
+    function submitTransaction(address destination, uint value,bytes calldata data)
+        public
+        returns (uint transactionId)
+        {
+            transactionId = addTransaction(destination,value,data);
+            // confirTransaction(transactionId);
+
+        }
+    // 
+    function confirmTransaction(uint transactionId) 
+        public
+        ownerExists(msg.sender)
+        transactionExists(transactionId)
+        notConfirmed(transactionId, msg.sender)
+    {
+
+    }    
 }
